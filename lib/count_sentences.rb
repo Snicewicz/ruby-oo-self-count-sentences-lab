0242983c54f63535c884937ce4254cb5075121fd
Snicewicz <snicewicz@gmail.com>
2022-01-25 14:38:09 -0500

    commit tree 40c8efae3c1b948eda238867f557de7ef811278a

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    if self.split(/[.*?*!*]/).count == 0
+      return 0
+    else 
+      split_array = self.split(/[.*?*!*]/)
+      split_array.delete_if{|x| x == ""}
+      return split_array.count
+    end
   end
 end
